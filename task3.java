2acdb0a4f7205355e77f48bf3734d8d00af4cfc1
ArturMingl <adiscov90@gmail.com>
2023-03-24 23:24:41 +0300

    task3 done

---
+++ task3.java
@@ -0,0 +1,54 @@
+package java_tasks.Seminar1dz;
+
+import java.util.Scanner;
+
+/**
+ * Реализовать простой калькулятор (+ - / *)
+Ввод числа ->
+Ввод знака ->
+Ввод числа ->
+ */
+
+public class task3 {
+
+    // static String scanOperations() {
+    //     Scanner scan = new Scanner(System.in);
+    //     String operations = scan.nextLine();
+    //     return operations;
+    // }
+
+    static int getResult(char opr, int num1, int num2) {
+        int result = 0;
+        switch (opr) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+
+            default:
+                System.out.println("Введите оператор: -, +, *, /");
+                break;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Введите превое число: ");
+        int num1 = reader.nextInt();
+        System.out.println("Введите оператор: -, +, *, /");
+        char operations = reader.next().charAt(0);
+        System.out.print("Введите второе число: ");
+        int num2 = reader.nextInt();
+        reader.close();
+        int result = getResult(operations, num1, num2);
+        System.out.printf("%d %s %d = %d", num1, operations, num2, result);
+    }
+}
